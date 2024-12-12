c36bbda039a57d2a10b69f3f2d76d9e33b0cc66e
manuelaraujoUEM <22298227@live.uem.es>
2024-12-12 16:09:10 +0100

    Leer linea y parsear linea del archivo

--- LogMaster.py
+++ LogMaster.py
@@ -21,6 +21,35 @@ def preprocess_date(date_str):
         date_str = date_str.replace(spanish, english)
     return date_str
 
+def parse_log_file(file_path):
+    """
+    Generador que lee el archivo de log línea por línea y analiza cada una.
+    """
+    with open(file_path, 'r') as file:
+        for line in file:
+            parsed_line = parse_log_line(line)
+            yield parsed_line, line
+
+def parse_log_line(line):
+    """analiza una línea del archivo y devuelve None si no es válida."""
+    try:
+        parts = line.strip().split()
+        if len(parts) != 3:
+            return None
+        timestamp = int(parts[0])
+        src_host = parts[1]
+        dst_host = parts[2]
+        return timestamp, src_host, dst_host
+    except (ValueError, IndexError):
+        return None
+
+def read_log_file(log_file, init_datetime,end_datetime,target_host,time_tolerance=300):
+    """
+        Lee un archivo de log desde un generador para intentar consumir menos memoria
+    """
+    init_time = int(init_datetime.timestamp()) - time_tolerance
+    end_time = int(end_datetime.timestamp()) + time_tolerance
+
 #rango de fechas
 init_datetime_str = "Martes, 13 de agosto de 2019 01:00:00"
 end_datetime_str = "Martes, 13 de agosto de 2019 21:00:00"
@@ -38,3 +67,11 @@ end_datetime = dt.datetime.strptime(end_datetime_str, datetime_format)
 
 print("Fecha de inicio: ", init_datetime)
 print("Fecha de fin: ", end_datetime)
+
+#cargar archivo
+log_file = 'test.txt'
+#Host a analizar
+host = 'Savhannah'
+
+print("Archivo a analizar: ", log_file)
+print("Host a analizar: ", host)
