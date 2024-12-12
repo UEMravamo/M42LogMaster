172bf402144aca0f125c890e43bbbf31934741a0
manuelaraujoUEM <22298227@live.uem.es>
2024-12-12 16:49:52 +0100

    lee logs de un archivo de prueba con host prueba y tiempo de prueba

--- LogMaster.py
+++ LogMaster.py
@@ -21,11 +21,11 @@ def preprocess_date(date_str):
         date_str = date_str.replace(spanish, english)
     return date_str
 
-def parse_log_file(file_path):
+def parse_log_file(log_file):
     """
     Generador que lee el archivo de log línea por línea y analiza cada una.
     """
-    with open(file_path, 'r') as file:
+    with open(log_file, 'r') as file:
         for line in file:
             parsed_line = parse_log_line(line)
             yield parsed_line, line
@@ -43,12 +43,35 @@ def parse_log_line(line):
     except (ValueError, IndexError):
         return None
 
-def read_log_file(log_file, init_datetime,end_datetime,target_host,time_tolerance=300):
+def read_log_file(log_file, init_,end_,target_host,time_tolerance=300):
     """
         Lee un archivo de log desde un generador para intentar consumir menos memoria
     """
-    init_time = int(init_datetime.timestamp()) - time_tolerance
-    end_time = int(end_datetime.timestamp()) + time_tolerance
+    init_ = int(init_.timestamp()) - time_tolerance
+    end_ = int(end_.timestamp()) + time_tolerance
+
+    conns = defaultdict(set)
+
+    lines = {"valid_lines": 0, "invalid_lines": 0, "out_of_range_lines": 0}
+
+    for parsed_line, raw_line in parse_log_file(log_file):
+        if parsed_line is None:
+            lines["invalid_lines"] += 1
+            continue
+
+        timestamp, src_host, dst_host = parsed_line
+        if not (init_ <= timestamp <= end_):
+            lines["out_of_range_lines"] += 1
+            continue
+
+        if src_host == target_host:
+            conns['outgoing'].add(dst_host)
+        if dst_host == target_host:
+            conns['incoming'].add(src_host)
+        lines["valid_lines"] += 1
+
+    return conns, lines
+
 
 #rango de fechas
 init_datetime_str = "Martes, 13 de agosto de 2019 01:00:00"
@@ -75,3 +98,10 @@ host = 'Savhannah'
 
 print("Archivo a analizar: ", log_file)
 print("Host a analizar: ", host)
+
+connections, stats = read_log_file(log_file, init_datetime, end_datetime, host)
+
+print(connections)
+print(stats)
+end = time.time();
+print("Tiempo", end-start)
