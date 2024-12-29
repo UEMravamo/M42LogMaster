5723481b3bddea458dcdae112388fe335be6dc55
daniuem <22060943@live.uem.es>
2024-12-29 12:00:05 +0100

    Add calculo de tiempo de CPU de la función y carga de CPU global + Calcular promedios de valores de uso de recursos

--- tests/pruebas recursos/test_grafo_resources.py
+++ tests/pruebas recursos/test_grafo_resources.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from datetime import datetime
 import psutil
 from functools import wraps
@@ -13,53 +14,95 @@ def measure_memory_and_cpu(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         process = psutil.Process()
+        
+        # Variables para acumular los sumatorios de medidas
+        memory_usage_total = 0
+        cpu_usage_function_total = 0
+        execution_time_total = 0
+        cpu_global_total_start = 0
+        cpu_global_total_end = 0
 
-        # Medimos la memoria usada antes de llamar a la función
-        start_memory = process.memory_info().rss / (1024 * 1024)  # Memoria en MB
+        # Repeticiones de las mediciones para luego hacer un promedio
+        repetitions = 30  
 
-        # Medimos el uso de CPU antes de ejecutar la función
-        start_cpu = process.cpu_percent(interval=2)
+        for _ in range(repetitions):
+            # Medimos la memoria usada ANTES de ejecutar la función
+            start_memory = process.memory_info().rss / (1024 * 1024)  # Memoria en MB
 
-        # Ejecutamos la función
-        result = func(*args, **kwargs)
+            # Medimos la carga de CPU global ANTES de ejecutar la función
+            cpu_global_total_start = psutil.cpu_percent(interval=2)
 
-        # Medimos la memoria usada después de llamar a la función
-        end_memory = process.memory_info().rss / (1024 * 1024)
+            # Medimos la carga de CPU del proceso ANTES de ejecutar la función
+            start_cpu_function = process.cpu_percent(interval=2)
 
-        # Medimos el uso de CPU después de ejecutar la función
-        end_cpu = process.cpu_percent(interval=2)
+            # Iniciamos el cronómetro para medir el tiempo de ejecución de la función
+            start_time = time.time()
 
-        # Calculamos la diferencia de memoria antes y después
-        memory_usage = end_memory - start_memory
+            # Ejecutamos la función ##########################################
+            result = func(*args, **kwargs)
+            #################################################################
 
-        # Calculamos la diferencia de uso de CPU antes y después
-        cpu_usage = end_cpu - start_cpu
+            # Medimos la memoria usada JUSTO DESPUÉS de terminar de ejecutar la función
+            end_memory = process.memory_info().rss / (1024 * 1024)
+
+            # Medimos la carga de CPU global JUSTO DESPUÉS de terminar de ejecutar la función
+            cpu_global_total_end = psutil.cpu_percent(interval=2)
+
+            # Medimos la carga de CPU del proceso JUSTO DESPUÉS de terminar de ejecutar la función
+            end_cpu_function = process.cpu_percent(interval=2)
+
+            # Detenemos el cronómetro para medir el tiempo de ejecución
+            end_time = time.time()
+
+            # Calculamos la diferencia de memoria antes y JUSTO DESPUÉS de ejecutar la función
+            memory_usage = end_memory - start_memory
+
+            # Calculamos el tiempo total de ejecución
+            execution_time = end_time - start_time
+
+            # Calculamos la diferencia de carga de CPU antes y JUSTO DESPUÉS de ejecutar la función
+            cpu_usage_function = end_cpu_function - start_cpu_function
+
+            # Calculamos la media de la carga de CPU global entre el valor antes y JUSTO DESPUÉS de ejecutar la función
+            cpu_global = (cpu_global_total_start + cpu_global_total_end) / 2
+
+            # Calculamos la diferencia de carga de CPU GLOBAL antes y JUSTO DESPUÉS de ejecutar la función# Incremento de la carga de CPU global
+            increment_cpu_global = cpu_global_total_end - cpu_global_total_start
+
+            # Sumamos los resultados a las variables sumatorias
+            memory_usage_total += memory_usage
+            execution_time_total += execution_time
+            cpu_usage_function_total += cpu_usage_function
+
+        # Calculamos los promedios
+        memory_usage_avg = memory_usage_total / repetitions
+        execution_time_avg = execution_time_total / repetitions
+        cpu_usage_function_avg = cpu_usage_function_total / repetitions
 
         # Imprimimos los resultados
+        print("\n----------------------------------------------------------------")
         print(f"Función: {func.__name__}")
-        print(f"Uso de memoria: {memory_usage:.4f} MB")
-        print(f"Uso de CPU: {cpu_usage:.4f}%")
+        print(f"Uso de memoria promedio: {memory_usage_avg:.4f} MB")
+        print(f"Tiempo de ejecución promedio: {execution_time_avg:.4f} s")
+        print(f"Carga de CPU de la función promedio: {cpu_usage_function_avg:.4f}%")
+        print(f"Carga de CPU global: {cpu_global:.4f}%")
+        print(f"Incremento de carga de CPU global: {increment_cpu_global:.4f}%")
+        print("----------------------------------------------------------------\n")
 
         return result
     return wrapper
 
-# Añadimos el decorador de @measure_memory_and_cpu a test_build_graph() para poder medir la memoria y CPU utilizadas antes y después de ejecutar la función
 @measure_memory_and_cpu
 def test_build_graph():
     log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'input-file-10000.txt'))
     return build_graph(log_file)
 
-# Añadimos el decorador de @measure_memory_and_cpu a test_find_connections_in_time_range() para poder medir la memoria y CPU utilizadas antes y después de ejecutar la función
 @measure_memory_and_cpu
 def test_find_connections_in_time_range(grafo):
-    # Definimos start_time y end_time como objetos datetime para pasarle a la función como rango de búsqueda
     start_time = datetime.fromtimestamp(1672531200)
     end_time = datetime.fromtimestamp(1672531300)
     return find_connections_in_time_range(grafo, 'hostname_example', start_time, end_time)
 
 if __name__ == "__main__":
-    # Llamamos a test_build_graph con el decorador para medir la memoria y CPU utilizadas antes y después de ejecutar la función
     grafo = test_build_graph()
-
-    # Llamamos a test_find_connections con el decorador para medir la memoria y CPU utilizadas antes y después de ejecutar la función
     test_find_connections_in_time_range(grafo)
