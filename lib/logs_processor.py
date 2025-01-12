a10054aa2aafc83661d247f293558273cb61a6bc
221c1129 <221c1129@live.uem.es>
2025-01-12 14:46:56 +0100

    Fix errores de import

--- lib/logs_processor.py
+++ lib/logs_processor.py
@@ -1,101 +1,16 @@
 # Librería para procesamiento y análisis de logs
-import datetime
+from datetime import datetime
 import itertools
 from collections import defaultdict
 import multiprocessing
 import os
 import networkx as nx
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, to_timestamp, window, count, desc, split, length
+from datetime import datetime
+import sys
 
-
-# Versión concurrente
-def process_chunk_binary(chunk, init_, end_, target_host):
-    """Procesa un fragmento binario decodificado como texto."""
-    conns_in, conns_out = defaultdict(int), defaultdict(int)
-    lines = chunk.splitlines()  # Divide en líneas
-
-    for line in lines:
-        try:
-            # Dividir línea en partes
-            parts = line.split()
-            # Convertir timestamp a segundos
-            timestamp = int(parts[0]) // 1000
-            # Extraer hosts
-            src_host, dst_host = parts[1], parts[2]
-        except (ValueError, IndexError):
-            # Si hay un error, pasar a la siguiente línea
-            continue
-
-        # Solo procesar si el timestamp está dentro del rango + 5 minutos
-        if (init_-300) <= timestamp <= (end_+300):
-            if src_host == target_host:
-                conns_out[dst_host] += 1
-            if dst_host == target_host:
-                conns_in[src_host] += 1
-
-    return conns_in, conns_out
-
-def merge_results(results):
-    """Combina los resultados parciales."""
-    final_in, final_out = defaultdict(int), defaultdict(int)
-
-    try:
-        for conns_in, conns_out in results:
-            for host, count in conns_in.items():
-                final_in[host] += count
-            for host, count in conns_out.items():
-                final_out[host] += count
-    except Exception as e:
-        raise RuntimeError(f"Error al combinar resultados: {e}")
-
-    return {'entrantes': final_in, 'salientes': final_out}
-
-def process_log_file_binary(file_, init_, end_, target_host, num_workers=None):
-    """
-    Procesa el archivo en modo binario, dividiéndolo en bloques, y decodifica las líneas.
-    """
-    if num_workers is None:
-        num_workers = os.cpu_count()
-
-    init_ = int(init_.timestamp())
-    end_ = int(end_.timestamp())
-    chunk_size = 1024 * 1024  # Leer en bloques de 1 MB
-
-    results = []
-    pool = multiprocessing.Pool(num_workers)
-    # Leer archivo en bloques
-    try:
-        with open(file_, 'rb') as f:
-            # Este trozo se explica en Explanation.md
-            leftover = b"" # Esto hace que sea una cadena de bytes
-            while chunk := f.read(chunk_size):
-                chunk = leftover + chunk
-                try:
-                    last_newline = chunk.rindex(b'\n')
-                    leftover = chunk[last_newline + 1:]
-                    chunk = chunk[:last_newline]
-                except ValueError:
-                    leftover = chunk
-                    continue
-                try:
-                    decoded_chunk = chunk.decode('utf-8')  # Decodificar como texto
-                except UnicodeDecodeError as e:
-                    print(f"Error al decodificar fragmento: {e}")
-                    continue
-
-                # Procesar el fragmento en paralelo
-                results.append(pool.apply_async(process_chunk_binary, (decoded_chunk, init_, end_, target_host)))
-    except (OSError, IOError) as e:
-        raise FileNotFoundError(f"Error al leer el archivo {file_} : {e}")
-    finally:
-        pool.close()
-        pool.join()
-
-    # Combinar resultados parciales
-    try:
-        processed_results = [res.get() for res in results]
-        return merge_results(processed_results)
-    except Exception as e:
-        raise RuntimeError(f"Error al combinar resultados: {e}")
+### Version secuencial ###
 
 ### Version Grafo ###
 # Función para leer el archivo de log y construir el grafo
@@ -113,7 +28,7 @@ def build_graph(log_file):
             host_to = parts[2]
 
             # Convertir timestamp a formato datetime
-            timestamp_datetime = datetime.datetime.utcfromtimestamp(timestamp / 1000)
+            timestamp_datetime = datetime.utcfromtimestamp(timestamp / 1000)
             timestamp_str = timestamp_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
             # Añadir nodos (hosts) al grafo
@@ -262,3 +177,56 @@ def process_log_file_binary(file_, init_, end_, target_host, num_workers=None):
         return merge_results(processed_results)
     except Exception as e:
         raise RuntimeError(f"Error al combinar resultados: {e}")
+    
+
+##Version Distribuida con Spark##
+
+#Asegurar que el directorio lib está en el PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
+
+from file_manager import preprocess_date
+
+def process_log_spark(log_file, hostname, start_datetime_str, end_datetime_str, datetime_format="%A, %d de %B de %Y %H:%M:%S"):
+    """
+    Procesa un archivo de log con spark para encontrar conexiones en un rango de tiempo
+
+    log_file(str): ruta al archivo de log
+    hostname(str): hostname para el cual buscar conexiones
+    start_datetime_str(str): cadena de fecha y hora de inicio
+    end_datetime_str(str): cadena de fecha y hora de fin
+    datetime_format(str): formato de la cadena de fecha y hora
+
+    dataframe: dataframe de spark con las conexiones encontradas
+    """
+    #inicializar sparksession
+    spark = SparkSession.builder \
+        .appName("LogProcessorSpark") \
+        .getOrCreate()
+    
+    start_datetime = datetime.strptime(preprocess_date(start_datetime_str), datetime_format)
+    end_datetime = datetime.strptime(preprocess_date(end_datetime_str), datetime_format)
+
+    #leer el archivo de log como un dataframe
+    df=spark.read.text(log_file)
+
+    df = df.select(
+        (col("value").substr(1, 10) * 1000).cast("long").alias("timestamp"),
+        col("value").substr(12, length(split(col("value"), " ")[1])).alias("host_from"),
+        split(col("value"), " ")[2].alias("host_to")
+    )
+
+    #Convertir timestamp a formato datetime
+    df = df.withColumn("datetime", to_timestamp(col("timestamp") / 1000))
+
+    #filtrar por rango de tiempo
+    df = df.filter((col("datetime") >= start_datetime) & (col("datetime") <= end_datetime))
+
+    #filtrar por hostname
+    connections_in = df.filter(col("host_to") == hostname)
+    connections_out = df.filter(col("host_from") == hostname)
+
+    #agrupar por host y contar conexiones
+    incoming_connections = connections_in.groupBy("host_from").agg(count("*").alias("count")).orderBy(col("count"), ascending=False)
+    outgoing_connections = connections_out.groupBy("host_to").agg(count("*").alias("count")).orderBy(col("count"), ascending=False)
+
+    return incoming_connections, outgoing_connections
